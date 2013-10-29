8a0a0f716272a286ffa66eeddf14c62c97aa1438
Damian <d.dlugosz@bestnetwork.it>
2013-10-29 13:58:20 +0100

    First commit

---
+++ Bestnetwork/GSMGateway/2n/Voiceblue/nVoiceblueBundle.php
@@ -0,0 +1,14 @@
+<?php
+namespace Bestnetwork\GSMGateway\2n\Voiceblue;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+use Bestnetwork\Telnet;
+
+/**
+ * Voiceblue GSM Gateway from 2n
+ *
+ * @author Bestnetwork <reparto.sviluppo@bestnetwork.it>
+ */
+class 2nVoiceblueBundle extends Bundle {
+}
